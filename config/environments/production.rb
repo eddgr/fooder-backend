278ea1c7ab45e8d99a4c63e0a42f04a36edd3a87
Edgar Ong <emailedgar@gmail.com>
2019-07-05 14:29:24 -0400

    add action cable url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
+  config.action_cable.url = 'wss://fooder-app-frontend.herokuapp.com/cable'
   config.web_socket_server_url = "wss://fooder-app-frontend.herokuapp.com/cable"
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
   config.action_cable.allowed_request_origins = ['https://fooder-app-frontend.herokuapp.com', 'http://fooder-app-frontend.herokuapp.com']
