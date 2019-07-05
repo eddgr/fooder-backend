743ddb7e34ab2f92e1181c386a1bcfbe36391ff3
Edgar Ong <emailedgar@gmail.com>
2019-07-05 14:47:53 -0400

    trying ws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,8 +34,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://fooder-app-frontend.herokuapp.com/cable'
-  config.web_socket_server_url = "wss://fooder-app-frontend.herokuapp.com/cable"
+  config.action_cable.url = 'ws://fooder-app-frontend.herokuapp.com/cable'
+  config.web_socket_server_url = "ws://fooder-app-frontend.herokuapp.com/cable"
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
   config.action_cable.allowed_request_origins = ['https://fooder-app-frontend.herokuapp.com', 'http://fooder-app-frontend.herokuapp.com']
 
